d64c05842b3daa2a4c7c4506f9da278bb6c19419
nensiramani66-design <nensiramani66@gmail.com>
2026-09-23 20:29:34 +0200

    Use sharper centered CEO portrait source

--- our-firm-refinement.js
+++ our-firm-refinement.js
@@ -185,7 +185,7 @@
       </div>
       <div class="kl-leadership-grid">
         <article class="kl-leader">
-          <div class="kl-leader-photo-frame"><img src="assets/ceo-portrait-final.webp" alt="Fictional CEO portrait"></div>
+          <div class="kl-leader-photo-frame"><img src="assets/portraits.webp" alt="Fictional CEO portrait"></div>
           <div class="kl-leader-copy">
             <div class="kl-leader-role" data-kl-text="ceoRole"></div>
             <h4>Dr. Alexander Kronberg</h4>
